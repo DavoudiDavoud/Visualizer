87bb00eddb3f918661a60505fe5d4363e946e2bc
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-07 21:33:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	window.showMaximized();
  
 	// call the window.timerEvent function every 40 ms
-	window.startTimer(20);
+	window.startTimer(40);
 
 	// execute the application
 	return app.exec();
